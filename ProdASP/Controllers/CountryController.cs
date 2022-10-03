2f037c43cb2a00aa43e9c862cb7a86b4feb37341
Georgi-Progger <hugaeag@gmail.com>
2022-10-03 22:50:51 +0300

    PlaceAdd

---
+++ ProdASP/Controllers/CountryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProdASP.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
