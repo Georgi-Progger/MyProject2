8ffe034c373d229b335862ff2214a50203805cbf
stud <stud@10-605-007>
2022-10-04 13:33:48 +0300

    Register

--- ProdASP/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ProdASP/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,7 +122,7 @@ namespace ProdASP.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, Enums.Roles.Roles.Basic.ToString());
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
